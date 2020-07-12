46d0d59f8d7f15cc03ab945bb73479f0fa395d86
Anders D. Johnson <anders.d.johnson.dev@gmail.com>
2020-07-12 06:04:08 -0500

    feat: build types scripts & tsconfig, devDeps to root

--- index.js
+++ index.js
@@ -2,26 +2,29 @@
 
 const createMonorepo = require("base-create-monorepo");
 
-createMonorepo("babel-typescript-monorepo", {
+createMonorepo({
   package: {
     keywords: ["create-babel-typescript-monorepo"],
     scripts: {
       start: "lerna run start",
       build: "lerna run build",
       "build:watch": "lerna run --parallel build:watch",
+      "build:types": "lerna run build:types",
+      "build:types:watch": "lerna run --parallel build:types:watch",
     },
   },
+  devDependencies: [
+    "@babel/core",
+    "@babel/cli",
+    "@babel/node",
+    "@babel/preset-env",
+    "@babel/preset-typescript",
+    "@babel/plugin-transform-runtime",
+    "typescript",
+    "@andersdjohnson/tsconfig",
+  ],
   eachPackage: {
     dependencies: ["@babel/runtime"],
-    devDependencies: [
-      "@babel/core",
-      "@babel/cli",
-      "@babel/node",
-      "@babel/preset-env",
-      "@babel/preset-typescript",
-      "@babel/plugin-transform-runtime",
-      "typescript",
-    ],
     package: {
       main: "dist/index.js",
       keywords: ["create-babel-typescript-monorepo"],
@@ -29,9 +32,23 @@ createMonorepo("babel-typescript-monorepo", {
         start: "node .",
         build: "babel -x .ts,.tsx src --out-dir dist",
         "build:watch": "npm run build -- --watch",
+        "build:types": "tsc --emitDeclarationOnly",
+        "build:types:watch": "npm run build:types -- --watch",
       },
     },
-    files: ["src/index.ts"],
+    files: [
+      "src/index.ts",
+      {
+        path: "tsconfig.json",
+        contents: {
+          extends: "../../tsconfig.json",
+          include: ["src"],
+          compilerOptions: {
+            outDir: "dist",
+          },
+        },
+      },
+    ],
   },
   files: [
     {
@@ -39,6 +56,15 @@ createMonorepo("babel-typescript-monorepo", {
       contents: {
         presets: ["@babel/preset-env", "@babel/preset-typescript"],
         plugins: ["@babel/plugin-transform-runtime"],
+      },
+    },
+    {
+      path: "tsconfig.json",
+      contents: {
+        extends: "@andersdjohnson/tsconfig",
+        compilerOptions: {
+          target: "esnext",
+        },
       },
     },
   ],
